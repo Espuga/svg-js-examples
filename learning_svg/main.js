df933d7ebceb5148c05fa8d33e6b4aa57ba48dbd
Espuga <marcespugamoreno@gmail.com>
2023-06-05 12:58:38 +0200

    lerning-svg

--- learning_svg/main.js
+++ learning_svg/main.js
@@ -1,19 +1,16 @@
-/* var draw = SVG().addTo('body').size(500, 300);
-draw.attr('id', 'canvas'); */
-
-/* import Session from 'svg-text-to-path'; */
 
 // Agafar ids
 var existingSvg = document.getElementById('canvas');
 var draw = SVG.adopt(existingSvg);
 var name_input = document.getElementById('name_input');
-//var text = draw.text("");
+var text = draw.text("");
+var text2 = draw.text("");
 
 var fontSize = document.getElementById('fontSize');
 var font = document.getElementById('font');
 var bt_download = document.getElementById('bt_download');
 
-
+/* 
 function centerFigure(){
     var svgWidth = draw.width();
     var svgHeight = draw.height();
@@ -35,43 +32,22 @@ function centerContorn(){
 
 var fontSizeValue = document.getElementById('fontSize').value;
 var contorn = draw.polygon('50,0 60,40 100,50 60,60 50,100 40,60 0,50 40,40');
-contorn.size((parseInt(fontSizeValue)+(parseInt(fontSizeValue)*0.2)).toString(),(parseInt(fontSizeValue)+(parseInt(fontSizeValue)*0.2)).toString());
+//var contorn = draw.rect(100, 100);
+contorn.size((parseInt(fontSizeValue)+50).toString(),(parseInt(fontSizeValue)+50).toString());
+contorn.attr({fill:'#fff'});
+contorn.stroke({color: 'black'});
 
 var figura = draw.polygon('50,0 60,40 100,50 60,60 50,100 40,60 0,50 40,40');
-figura.fill();
-figura.stroke({ color: 'red' });
+//var figura = draw.rect(100, 100);
+//figura.fill();
+figura.stroke({ color: 'black' });
 figura.size(fontSizeValue,fontSizeValue);
-figura.attr({fill:'#f06'});
+figura.attr({fill:'#fff'});
 centerContorn()
-centerFigure() 
+centerFigure()  */
 
 
 
-
-
-
-fontSize.addEventListener('change', function(){
-    var fontSizeValue = this.value;
-    contorn.size((parseInt(fontSizeValue)+50).toString(),(parseInt(fontSizeValue)+50).toString());
-    centerContorn()
-    figura.size(fontSizeValue, fontSizeValue);
-    centerFigure()
-    
-});
-
-
-// Listener text field
-name_input.addEventListener('input', function(){
-    /* var textValue = this.value;
-    var pathData = Session.convert(textValue);
-    var pathElement = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    pathElement.setAttribute("d", pathData);
-    pathElement.setAttribute("fill", "blue");
-    pathElement.setAttribute("stroke", "red");
-    pathElement.setAttribute("filter", "drop-shadow(2px 2px 2px gray)");
-    draw.appendChild(pathElement); */
-
-});
 
 
 
@@ -108,10 +84,10 @@ bt_download.addEventListener('click', function(){
 });
 
 
-
-/* function changePosition(draw, text, from){
+function changePosition(draw, text, from){
     var svgWidth = draw.width();
     var svgHeight = draw.height();
+    // text
     var textWidth = text.bbox().width;
     var textHeight = text.bbox().height;
     if(from == "name_input"){
@@ -119,32 +95,56 @@ bt_download.addEventListener('click', function(){
     }else{
         text.move(((svgWidth-textWidth)/2), ((svgHeight-textHeight)/2));
     }
+    //text.move(((svgWidth-textWidth)/2), ((svgHeight-textHeight)/2));
     text.attr({ 'text-anchor': 'middle', 'dominant-baseline': 'middle' });
-}; */
+    // text2
+    /* var text2Width = text2.bbox().width;
+    var text2Height = text2.bbox().height;
+    if(from == "name_input"){
+        text2.move(((svgWidth-text2Width)/2)+(text2Width/2), ((svgHeight-text2Height)/2)-(text2Height/2));
+    }else{
+        text2.move(((svgWidth-text2Width)/2), ((svgHeight-text2Height)/2));
+    }
+    text2.attr({ 'text-anchor': 'middle', 'dominant-baseline': 'middle' }); */
+};
 
 
 // Listener text field
-/* name_input.addEventListener('input', function(){
-    text.clear();
+name_input.addEventListener('input', function(){   
     var textValue = document.getElementById('name_input').value;
     var valueSize = document.getElementById('fontSize').value;
     var valueFont = document.getElementById('font').value;
+
+    text2.clear()
+    text2 = draw.text(textValue);
+    text2.move(0, 0).font({ fill: '#f06', family: valueFont, size: parseInt(valueSize)+20 });
+    text2.attr({fill:'#fff'});
+    text2.stroke({color:'black'});
+
+    text.clear();
     text = draw.text(textValue);
     text.move(0, 0).font({ fill: '#f06', family: valueFont, size: valueSize });
+    text.attr({fill:'#fff'});
+    text.stroke({color:'black'});
+
+    
+
     changePosition(draw, text, "name_input");
-}); */
+    changePosition(draw, text2, "name_input");
+
+});
 
 
 // Listener font size
-/* fontSize.addEventListener('change', function(){
-    var fontSizeValue = document.getElementById('fontSize').value;
+fontSize.addEventListener('change', function(){
+    var fontSizeValue = this.value;
     text.font({size: fontSizeValue});
     changePosition(draw, text, "fontSize");
-}); */
+});
 
 // Listener font
-/* font.addEventListener('change', function(){
+font.addEventListener('change', function(){
     var fontValue = document.getElementById('font').value;
     text.font({family:fontValue});
     changePosition(draw, text, "font");
-}) */
+});
